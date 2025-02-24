e6010e490c184c0dbc1fab5d19f46072d8b0a5a7
MSachin11 <sachinmathanprabhu11@gmail.com>
2025-02-24 13:21:12 +0530

    Added full project

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import json
 from datetime import datetime
+if st.button("🔙 Back to InnovNutriBite"):
+    st.markdown("[Go Back to App](https://innovnutribite.web.app/)", unsafe_allow_html=True)
 
 # Load food database
 def load_food_database():
